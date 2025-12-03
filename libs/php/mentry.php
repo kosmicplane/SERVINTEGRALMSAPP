7aec5d23bb6fe3d933ed9bd288da23896d588012
Juan Esteban Beron Zapata <aeroberon@gmail.com>
2025-12-03 14:37:27 +0800

    Update mentry.php

--- libs/php/mentry.php
+++ libs/php/mentry.php
@@ -1,6 +1,9 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// No mostrar notices ni warnings en la salida (solo loguearlos)
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+
 
 class entryPoint{
     private $params;
