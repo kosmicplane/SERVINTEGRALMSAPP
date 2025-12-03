26ff0111de77e044994e98f9f368551bb506c368
Juan Esteban Beron Zapata <aeroberon@gmail.com>
2025-12-03 14:47:32 +0800

    Update mentry.php

--- libs/php/mentry.php
+++ libs/php/mentry.php
@@ -1,9 +1,8 @@
 <?php
-// No mostrar notices ni warnings en la salida (solo loguearlos)
+// No mostrar notices ni warnings en la salida (que rompen el JSON)
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
-
 
 class entryPoint{
     private $params;
